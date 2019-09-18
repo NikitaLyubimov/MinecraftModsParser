11a90ae2f36ab552d4054f682b34bfed94851479
NikitaLyubimov <sobachiy.deputat2017@yandex.ru>
2019-09-18 22:44:47 +0300

    Added file with database logic

---
+++ database_tools.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="127.0.0.1",
+                    user="root",
+                    passwd="gachimuchicriticale99099",
+                    db="parser")
+cursor = db.cursor()
+
+def insert_post(content_arr):
+    global cursor
+
+    cursor.execute(f"INSERT INTO minecraft_modes(title, description, link) VALUES({content_arr[0]}, {content_arr[1]}, {content_arr[2]})")
+
+
